9dd879a31c5bc5f2fc782359034ae1b2e9933ef4
yvzkr <yavuzkuru3@gmail.com>
2019-11-10 21:49:30 +0300

    tsp parametreler yazıldı

---
+++ tspmain.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 21:42:00 2019
+
+@author: yavuz
+"""
+
+
+def main():
+    '''parametreler'''
+    T = 100000
+    stopping_T = 0.0001
+    colling_faktor = 0.9995
+    stopping_iter = 10000000
+
+
+
+
+if __name__ == "__main__":
+    main()
